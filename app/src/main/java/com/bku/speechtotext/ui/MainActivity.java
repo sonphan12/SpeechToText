d7348748106a413a96f8572e1702f9b465a63dc6
Phan Tran Thai Son <phantranthaison12@gmail.com>
2018-11-11 16:09:23 +0700

    Add function to record

--- app/src/main/java/com/bku/speechtotext/ui/MainActivity.java
+++ app/src/main/java/com/bku/speechtotext/ui/MainActivity.java
@@ -1,17 +1,82 @@
 package com.bku.speechtotext.ui;
 
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.bku.speechtotext.R;
+import com.emrekose.recordbutton.OnRecordListener;
+import com.emrekose.recordbutton.RecordButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String FILE_NAME = "SpeechToText.3gp";
+    private static String TAG = "MainActivity";
+    @BindView(R.id.btnRecord)
+    RecordButton btnRecord;
 
+    private MediaRecorder mRecorder;
+    private String audioPath;
+    private boolean recordStarted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initRecorder();
+        btnRecord.setRecordListener(new OnRecordListener() {
+            @Override
+            public void onRecord() {
+                if (!recordStarted) {
+                    startRecord();
+                    recordStarted = true;
+                }
+            }
+
+            @Override
+            public void onRecordCancel() {
+                onRecordFinish();
+            }
+
+            @Override
+            public void onRecordFinish() {
+                finishRecord();
+                recordStarted = false;
+            }
+        });
+    }
+    private void startRecord() {
+        audioPath = Environment.getExternalStorageDirectory().getPath() + "/" + FILE_NAME;
+
+        mRecorder.setOutputFile(audioPath);
+        try {
+            mRecorder.prepare();
+            mRecorder.start();
+        } catch (Exception e) {
+            Log.w(TAG, e);
+        }
+    }
+
+    private void finishRecord() {
+        try {
+            mRecorder.stop();
+            mRecorder.release();
+        } catch (Exception e) {
+            Log.w(TAG, e);
+        }
+    }
+
+    private void initRecorder() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
     }
 }
 
