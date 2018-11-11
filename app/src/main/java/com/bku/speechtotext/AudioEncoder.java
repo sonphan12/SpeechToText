8516827dc901202e1b0404b3d84d5e921f0a0752
Phan Tran Thai Son <phantranthaison12@gmail.com>
2018-11-11 13:30:17 +0700

    Add AudioEncoder

---
+++ app/src/main/java/com/bku/speechtotext/AudioEncoder.java
@@ -0,0 +1,34 @@
+package com.bku.speechtotext;
+
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+public class AudioEncoder {
+    private static String TAG = "AudioEncoder";
+    public static String encodeBase64(String path) {
+        byte[] audioBytes;
+        String audioBase64 = "";
+        try {
+
+            File audioFile = new File(path);
+            long fileSize = audioFile.length();
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            FileInputStream fis = new FileInputStream(audioFile);
+            byte[] buf = new byte[1024];
+            int n;
+            while (-1 != (n = fis.read(buf)))
+                baos.write(buf, 0, n);
+            audioBytes = baos.toByteArray();
+
+            audioBase64 = Base64.encodeToString(audioBytes, Base64.DEFAULT);
+        } catch (Exception e) {
+            Log.w(TAG, e);
+        }
+        return audioBase64;
+    }
+}
