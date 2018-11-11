d95c13f5e4be125fee9674ff7224da59a12cb4fd
Kurosaki12a1 <tyblackzs@gmail.com>
2018-11-11 13:58:03 +0700

    Set up instance retrofit

--- app/src/main/java/com/bku/speechtotext/MainActivity.java
+++ app/src/main/java/com/bku/speechtotext/MainActivity.java
@@ -3,11 +3,47 @@ package com.bku.speechtotext;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
+    private final String CLOUD_API_KEY = "ABCDEF1234567890";
+
+    private static String url = "https://speech.googleapis.com/";
+
+    private static Retrofit retrofit = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public static Retrofit getRetrofit(){
+        if(retrofit == null){
+            Retrofit.Builder builder=new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create());
+
+            OkHttpClient.Builder httpClientBuilder=new OkHttpClient().newBuilder()
+                    .connectTimeout(60, TimeUnit.SECONDS)
+                    .readTimeout(60, TimeUnit.SECONDS)
+                    .writeTimeout(60,TimeUnit.SECONDS);
+
+            httpClientBuilder.addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY));
+
+            retrofit=builder.client(httpClientBuilder.build()).build();
+        }
+        return retrofit;
+    }
 }
+
+
+
